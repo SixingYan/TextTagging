4bc0515cf9cf52d56587e94d03d153816a2ec799
Yan, Sixing <plutoyem@outlook.com>
2019-07-01 17:57:09 +0800

    update

--- preprocess.py
+++ preprocess.py
@@ -5,16 +5,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
 每一行数据 1 2 3 4 5 /t a a b b o 
 
 
@@ -25,3 +15,9 @@
 
 
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
