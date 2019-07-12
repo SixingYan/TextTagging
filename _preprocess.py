a3cb23ca37a7b352fbd0cbe2f47cc675e13d6874
Yan, Sixing <plutoyem@outlook.com>
2019-07-12 17:24:25 +0800

    update

--- _preprocess.py
+++ _preprocess.py
@@ -1,44 +1,68 @@
 
+import os
+import const
 
-在这里就处理成ix了
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
 
-每一行数据 1 2 3 4 5 / t a a b b o
 
-
-需要统计最大长度
+def _parse_str_tag(string):
+    paras = string.split()
+    words = []
+    tags = []
+    for p in paras:
+        word, tag = p.split('/')
+        chars = list(word.split('_'))
+        words += chars
+        tags += [tag] * len(chars)
+    return words, tags
 
 
 def preprocess():
-    先转化成 空格 分割
 
-    trn_X
-    trn_y
+    print('start...')
+    trn_X = []
+    trn_y = []
+    with open(os.path.join(const.DATAPATH, 'train.txt'), 'r') as f:
+        for line in f:
+            words, tags = _parse_str_tag(line)
+            trn_X.append(' '.join(words))
+            trn_y.append(' '.join(tags))
 
-    tst_X
+    with open(os.path.join(const.DATAPATH, 'test.txt'), 'r') as f:
+        tst_X = [line.replace('_', ' ') for line in f.readlines()]
 
-    with open('') as f:
-        sent = f.readlines()
-    corpus = DataFrame({'sent': sent}).process_apply(lambda x: ' '.join(x.split('_'))).values.tolist()
+    with open(os.path.join(const.DATAPATH, 'corpus.txt'), 'r') as f:
+        corpus = [line.replace('_', ' ') for line in f.readlines()]
+
+    print('load complete ')
+    MAX_LEN = max([len(line) - line.count(' ') for line in tst_X])
 
     # 这里再生成词表
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(trn_X + tst_X + corpus)  # two list combine
-    trn_X_token = tokenizer.texts_to_sequences(trn)
-    trn_X_token = pad_sequences(trn_token, maxlen=MAX_LEN, padding='post')
+    trn_X_token = tokenizer.texts_to_sequences(trn_X)  # word to id
+    trn_X_token = pad_sequences(trn_X_token, maxlen=MAX_LEN, padding='post', value=0)
 
-    tst_X_token = tokenizer.texts_to_sequences(tst)
-    tst_X_token = pad_sequences(trn_token, maxlen=MAX_LEN, padding='post')
-
-    toPickle(os.path.join(const.DATAPATH, ''), word_to_ix)
+    tst_X_token = tokenizer.texts_to_sequences(tst_X)
+    tst_X_token = pad_sequences(tst_X_token, maxlen=MAX_LEN, padding='post', value=0)
 
     word_to_ix = tokenizer.word_index
-    word_to_ix[PAD_TAG] = 0
+    word_to_ix[const.PAD_TAG] = 0
 
-    ix_to_word = {ix: w for w, ix in tokenizer.word_index.items()}
+    #ix_to_word = {ix: w for w, ix in tokenizer.word_index.items()}
+    
+    toPickle(os.path.join(const.DATAPATH, 'trn_X_token.pickle'), trn_X_token)
+    toPickle(os.path.join(const.DATAPATH, 'tst_X.pickle'), tst_X)
+    toPickle(os.path.join(const.DATAPATH, 'tst_X_token.pickle'), tst_X_token)
+    toPickle(os.path.join(const.DATAPATH, 'corpus.pickle'), corpus)
+    toPickle(os.path.join(const.DATAPATH, 'word_to_ix.pickle'), word_to_ix)
 
 
 def toPickle(path, var):
-    pass
+    with open(path, 'wb') as f:
+        pickle.dump(var,f)
 
 
 def fromPickle():
@@ -46,4 +70,4 @@ def fromPickle():
 
 
 if __name__ == '__main__':
-    main()
+    preprocess()
--- const.py
+++ const.py
@@ -2,6 +2,11 @@ PAD_TAG = "<PAD>"
 START_TAG = "<START>"
 STOP_TAG = "<STOP>"
 
+import os
+BASEPATH = os.getcwd()
+DATAPATH = os.path.join(BASEPATH, 'Data/')
+MODELPATH = os.path.join(BASEPATH, 'Model/')
+
 
 TAG_TO_IX = {"a": 0, "b": 1, "c": 2, "o": 3,
              START_TAG: 4, STOP_TAG: 5, PAD_TAG: 6}
